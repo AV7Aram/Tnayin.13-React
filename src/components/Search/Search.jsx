4a5e55ee06b8c002a10f8ed26e2dfd6010ad1716
AV_Aram <aram.vardanyan.07@bk.ru>
2025-05-19 03:57:20 +0400

    Search form was implemented with the formik

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,10 +1,27 @@
+import { useFormik } from 'formik';
 import style from './Search.module.css';
 
 export function Search() {
+    const formik = useFormik({
+        initialValues: {
+            searchTerm: '', 
+        },
+        onSubmit: (values) => {
+            console.log(values.searchTerm);
+        },
+    });
+
     return (
         <div className={style.headerSearch}>
-            <form action="">
-                <input type="search" placeholder='Որոնում'/>
+            <form onSubmit={formik.handleSubmit}>
+                <input
+                    type="search"
+                    name="searchTerm"
+                    placeholder="Որոնում"
+                    value={formik.values.searchTerm}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                />
                 <i className={`bi bi-search ${style['bi-search']}`}></i>
             </form>
         </div>
